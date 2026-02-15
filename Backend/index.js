1e8ac5618e9aceca0c4aaeb71cf6d89887f21ef6
SayandeepBera <sayandeep4002@gmail.com>
2026-02-16 02:37:43 +0530

    Fix CORS for production

--- Backend/index.js
+++ Backend/index.js
@@ -24,7 +24,10 @@ initCronTasks();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
--- Backend/socket.js
+++ Backend/socket.js
@@ -7,6 +7,7 @@ const initSocket = (server) => {
     io = new Server(server, {
         cors: {
             origin: process.env.FRONTEND_URL,
+            methods: ["GET", "POST"],
             credentials: true
         }
     });
