6a3b7764bccb4f21da13c145e90d50eed355765d
SayandeepBera <sayandeep4002@gmail.com>
2026-02-16 00:57:58 +0530

    update few thing for deploy

--- Frontend/src/Components/LoginRegisterLayout.jsx
+++ Frontend/src/Components/LoginRegisterLayout.jsx
@@ -7,53 +7,16 @@ const LoginRegisterLayout = () => {
   const [toggleAnimation, setToggleAnimation] = useState(false);
 
   return (
-    <>
-      <div className="form-body">
-        <div className={`wrapper ${toggleAnimation ? "active" : ""}`}>
-          {/* Background animation for login & register */}
-          <span className="bg-animation"></span>
-          <span className="bg-animation2"></span>
+    <div className="form-body">
+      <div className={`wrapper ${toggleAnimation ? "active" : ""}`}>
+        {/* Background animation for login & register */}
+        <span className="bg-animation"></span>
+        <span className="bg-animation2"></span>
 
-          <Login setToggleAnimation={setToggleAnimation} />
-          <Register setToggleAnimation={setToggleAnimation} />
-        </div>
+        <Login setToggleAnimation={setToggleAnimation} />
+        <Register setToggleAnimation={setToggleAnimation} />
       </div>
-      <div className="flex justify-evenly items-center text-white mt-5">
-        <div className="">
-          <h4>User credentials</h4>
-          <div>
-            <p>Username: Rahul</p>
-            <p>Password: Rahul4002</p>
-          </div>
-        </div>
-
-        <div className="">
-          <h4>Tour Guide credentials</h4>
-          <div>
-            <p>Username: ApprovedGuide1</p>
-            <p>Password: Guide1_2025</p>
-          </div>
-
-          <div>
-            <p>Username: ApprovedGuide2</p>
-            <p>Password: Guide2_2025</p>
-          </div>
-
-          <div>
-            <p>Username: PendingGuide1</p>
-            <p>Password: Guide_P2025</p>
-          </div>
-        </div>
-
-        <div>
-          <h4>Admin credentials</h4>
-          <div>
-            <p>Username: Admin#00@</p>
-            <p>Password: Tour#321@</p>
-          </div>
-        </div>
-      </div>
-    </>
+    </div>
   )
 }
 
