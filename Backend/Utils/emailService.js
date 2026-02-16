9faae11ff8ae4a45eb8deae52875445b82dfd9ab
SayandeepBera <sayandeep4002@gmail.com>
2026-02-16 11:55:11 +0530

    Update email service transport configuration for deploy

--- Backend/Utils/emailService.js
+++ Backend/Utils/emailService.js
@@ -3,7 +3,9 @@ import nodemailer from 'nodemailer';
 export const sendNotificationEmail = async (email, subject, htmlContent) => {
     try {
         const transporter = nodemailer.createTransport({
-            service: "gmail",
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false,
             auth: {
                 user: process.env.ADMIN_EMAIL,
                 pass: process.env.PASSWORD,
